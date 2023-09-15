8528b75296aa48ca305e51d1d16e1175dd5726bf
Jacob Graham <jacobgraham2025@u.northwestern.edu>
2023-09-15 15:22:56 -0500

    Complted generate_mutator_commands.py

--- make_psf_for_namd/make_psf_multi_mutations.sh
+++ make_psf_for_namd/make_psf_multi_mutations.sh
@@ -11,7 +11,7 @@ module purge all
 module load python/anaconda3.6
 
 # Organize files into an output directory.
-out_dir=psfgen_out
+out_dir=test
 if [ ! -d "$out_dir" ];then
 	mkdir $out_dir
 else
@@ -21,17 +21,37 @@ else
 fi
 
 # Append necessary mutations to psfgen.tcl
-
+source activate /projects/p31412/Mfp_Brushes/envs/fga_mfp
 python generate_mutator_commands.py
 
-$mutant_commands = # String output from generate_mutator_commands.py
-sed 's$# This line will be replaced by mutator plugin commands$'"$mutant_commands"'$g' < psfgen.tcl > psfgen_mutate.tcl
+names=()
 
-# Generate psf files.
-module load vmd
-vmd -dispdev text -e psfgen_mutate.tcl >> psfgen.log
+# Read the list of names corresponding to each sequence line by line and add each line to the array.
+while IFS= read -r line; do
+	names+=("$line")
+done < "names.txt"
 
-# Organize files.
+# Do some stuff for each simulation.
+for element in "${names[@]}"; do
+	sim_dir=$out_dir/$element
+	mkdir $sim_dir
+	mutant_commands=$(cat "${element}_mutator_commands.txt") # String output from generate_mutator_commands.py
+	replace_string="# This line will be replaced by mutator plugin commands"
+	sed "s/$replace_string/$mutant_commands/g" < psfgen.tcl > psfgen_mutate.tcl
+	
+	
+	mv psfgen_mutate.tcl ${element}_mutator_commands.txt $sim_dir 
+	echo "$element"
+done
 
-# mv nfp5_cfp5* cfp5_updated_xyz.pdb psfgen.log ionized* $out_dir
-# cp view.vmd $out_dir
+
+
+
+# # Generate psf files.
+# module load vmd
+# vmd -dispdev text -e psfgen_mutate.tcl >> psfgen.log
+
+# # Organize files.
+
+# # mv nfp5_cfp5* cfp5_updated_xyz.pdb psfgen.log ionized* $out_dir
+# # cp view.vmd $out_dir
