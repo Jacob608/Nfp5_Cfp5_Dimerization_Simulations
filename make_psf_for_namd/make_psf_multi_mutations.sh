da9a3bbda776270533c6ea0453283c6cac0ba267
Jacob608 <81713611+Jacob608@users.noreply.github.com>
2023-10-19 14:36:25 -0500

    Update make_psf_multi_mutations.sh

--- make_psf_for_namd/make_psf_multi_mutations.sh
+++ make_psf_for_namd/make_psf_multi_mutations.sh
@@ -3,51 +3,48 @@ module purge all
 module load python/anaconda3.6
 module load vmd
 
-# Organize files into an output directory.
+#----- Make a directory into which files will be organized.
 out_dir=mutation_simulations
 if [ ! -d "$out_dir" ];then
 	mkdir $out_dir
-else
+else # If a directory by the name $out_dir already exists, exit with message.
 	echo "Directory ${out_dir} already exists."
 	echo "Exiting to avoid directory overwrite."
 	exit
 fi
 
-# Generate a simulation with no mutations
+#----- Generate a simulation with no mutations.
 
-mkdir $out_dir/no_mutation
-vmd -dispdev text -e combine_nfp5_cfp5.tcl >> combine_nfp5_cfp5.log
-vmd -dispdev text -e solvate_ionize.tcl >> solvate_ionize.log
-cp view.vmd $out_dir/no_mutation
-mv nfp5_cfp5* ionized* solvate_ionize.log combine_nfp5_cfp5.log $out_dir/no_mutation
+mkdir $out_dir/no_mutation # Make a directory to organize output files.
+vmd -dispdev text -e combine_nfp5_cfp5.tcl >> combine_nfp5_cfp5.log # Use VMD to position the two proteins next to each other in the same pdb/psf.
+vmd -dispdev text -e solvate_ionize.tcl >> solvate_ionize.log # Use VMD to add solvent and ions.
+cp view.vmd $out_dir/no_mutation # Copy view.vmd into output directory.
+mv nfp5_cfp5* ionized* solvate_ionize.log combine_nfp5_cfp5.log $out_dir/no_mutation # Organize files into the output directory.
 
-# Append necessary mutations to psfgen.tcl
-source activate /projects/p31412/Mfp_Brushes/envs/fga_mfp
-python generate_mutator_commands.py
-
-names=()
+#----- Make simulations with one protein mutated.
+source activate /projects/p31412/Mfp_Brushes/envs/fga_mfp # Activate a conda environment with Python version 3.6.0
+python generate_mutator_commands.py # Run generate_mutator_commands.py
 
 # Read the list of names corresponding to each sequence line by line and add each line to the array.
+names=()
 while IFS= read -r line; do
 	names+=("$line")
 done < "names.txt"
 
-# Do some stuff for each simulation.
+# Generate each simulation.
 for element in "${names[@]}"; do
-	sim_dir=$out_dir/$element
-	mkdir $sim_dir
+	sim_dir=$out_dir/$element 
+	mkdir $sim_dir # Make a directory to organize output files.
 
-	vmd -dispdev text -e combine_nfp5_cfp5.tcl >> combine_nfp5_cfp5.log
+	vmd -dispdev text -e combine_nfp5_cfp5.tcl >> combine_nfp5_cfp5.log # Use VMD to position the two proteins next to each other in the same pdb/psf.
 
 	mutant_commands=$(cat "${element}_mutator_commands.txt") # String output from generate_mutator_commands.py
-	# replace_string='This line will be replaced by mutator plugin commands'
-	# sed "s/${replace_string}/${mutant_commands}" < psfgen.tcl > psfgen_mutate.tcl
-	cat mutate.tcl ${element}_mutator_commands.txt > ${element}_mutate.tcl
-	# Generate psf files.
-	vmd -dispdev text -e ${element}_mutate.tcl >> ${element}_mutate.log
-	vmd -dispdev text -e solvate_ionize.tcl >> solvate_ionize.log
 
-	cp view.vmd $sim_dir
-	mv solvate_ionize.log combine_nfp5_cfp5.log nfp5_cfp5* cfp5_updated_xyz.pdb ionized* ${element}* $sim_dir 
-	echo "$element"
+	cat mutate.tcl ${element}_mutator_commands.txt > ${element}_mutate.tcl # Concatenate the VMD Mutator Plugin commands to the end of ${element}_mutator_commands.txt and save the output file as ${element}_mutate.tcl
+
+	vmd -dispdev text -e ${element}_mutate.tcl >> ${element}_mutate.log # Add mutations using VMD.
+	vmd -dispdev text -e solvate_ionize.tcl >> solvate_ionize.log # Use VMD to add solvent and ions.
+
+	cp view.vmd $sim_dir # Copy view.vmd into output directory.
+	mv solvate_ionize.log combine_nfp5_cfp5.log nfp5_cfp5* cfp5_updated_xyz.pdb ionized* ${element}* $sim_dir # Organize files into the output directory.
 done
