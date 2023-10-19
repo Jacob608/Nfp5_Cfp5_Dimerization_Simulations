d44b02eec9d034485d8e1d482c4ae9c613ef9f64
Jacob Graham <jacobgraham2025@u.northwestern.edu>
2023-10-19 16:39:46 -0500

    Increased water padding to 20 from 12.

--- make_psf_for_namd/solvate_ionize.tcl
+++ make_psf_for_namd/solvate_ionize.tcl
@@ -4,7 +4,7 @@ package require autoionize
 mol new nfp5_cfp5.psf
 mol addfile nfp5_cfp5.pdb
 
-solvate nfp5_cfp5.psf nfp5_cfp5.pdb -t 12 -o nfp5_cfp5_solvated
+solvate nfp5_cfp5.psf nfp5_cfp5.pdb -t 20 -o nfp5_cfp5_solvated
 
 autoionize -psf nfp5_cfp5_solvated.psf -pdb nfp5_cfp5_solvated.pdb -sc 0.15 -cation SOD -anion CLA
 
