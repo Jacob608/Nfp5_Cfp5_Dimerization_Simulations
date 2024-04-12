f3925c60059c698a4e6d15369df7955393cd6e83
Jacob608 <81713611+Jacob608@users.noreply.github.com>
2024-04-12 16:18:41 -0500

    Update run_namd.sh

--- run_simulation/run_namd.sh
+++ run_simulation/run_namd.sh
@@ -10,4 +10,4 @@
 module purge all
 module load namd
 
-/software/NAMD/2.13/verbs/charmrun /software/NAMD/2.13/verbs/namd2 +p52 run.namd > run_namd.log
+/software/NAMD/2.13/verbs/charmrun /software/NAMD/2.13/verbs/namd2 +p$SLURM_NPROCS run.namd > run_namd.log
