ab4dafb6cc04c1b1fe4632c0c7077c478d86d60a
Jacob608 <81713611+Jacob608@users.noreply.github.com>
2024-05-03 14:30:55 -0500

    Update restart_all_short_queue.sh

--- restart_all_short_queue.sh
+++ restart_all_short_queue.sh
@@ -1,9 +1,13 @@
+# Load necessary modules.
 module load python/anaconda3.6
+
+# Load all the simulation names stored in file names.txt into the list names.
 names=()
 while IFS= read -r line; do
 	names+=("$line")
 done < "names.txt"
 
+# For each element in names, navigate into that directory, copy auto_submit_restart_short_queue.py into that directory, replace 'simulation name' with the element in auto_submit_restart_short_queue.py and run auto_submit_restart_short_queue.py.
 for element in "${names[@]}"; do
 	cd $element
 	cp ../auto_submit_restart_short_queue.py .
