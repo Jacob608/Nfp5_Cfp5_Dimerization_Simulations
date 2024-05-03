169bdce24968be6905d3d4cf4902150644391ecc
Jacob608 <81713611+Jacob608@users.noreply.github.com>
2024-05-03 13:56:30 -0500

    Update get_NAMDenergy_all_simulations.sh

--- get_NAMDenergy_all_simulations.sh
+++ get_NAMDenergy_all_simulations.sh
@@ -6,12 +6,16 @@
 #SBATCH --ntasks-per-node=1  ## number of cores
 #SBATCH -t 4:00:00
 
+# Load necessary modules.
 module load vmd
+
+# Load all the simulation names stored in file names.txt into the list names.
 names=()
 while IFS= read -r line; do
 	names+=("$line")
 done < "names.txt"
 
+# For each element in names, navigate into that directory, copy get_NAMDenergy.vmd into that directory, and execute get_NAMDenergy.vmd in VMD.
 for element in "${names[@]}"; do
 	cd $element
 	cp ../get_NAMDenergy.vmd .
