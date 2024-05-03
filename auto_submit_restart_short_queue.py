c3877e46de512de1788779911d236d5adcfccb07
Jacob608 <81713611+Jacob608@users.noreply.github.com>
2024-05-03 14:48:22 -0500

    Update auto_submit_restart_short_queue.py

--- auto_submit_restart_short_queue.py
+++ auto_submit_restart_short_queue.py
@@ -1,19 +1,30 @@
+# Import necessary libraries.
 import os
 
+# Specify how many short queue simulations will be submitted by setting start and loop variables.
 start = 0
 loop = 134
+
+# Set the number of nodes and processesors to be requested in each short queue simulation.
 node_no = 1
 proc_no = 52
-# proc_no = 10
+
+# Set the number of MD steps to be executed by the run command in each simulation.
 steps_per_sim = 1500000
 # steps_per_sim = 100
+
+# Specify the name of this simulation by setting the variable name.
 name = 'simulation_name'
+
+# Run through a loop to set up each short queue simulation.
 for i in range(start,loop):
     print(i)
+
+    # Set variables to specify namd log file name and job name.
     log_filename = "run_namd_"+ str(i)+ ".log"
     jobname = "nbf_"+name+"_i"+str(i)
     
-    # Write submit file
+    # Write the submit file, named with variable filename, line by line.
     filename = "run_namd_"+ str(i) +".sh"
     f = open(filename, "w")
     f.write('#!/bin/bash\n')
@@ -29,12 +40,20 @@ for i in range(start,loop):
     f.write('#https://researchcomputing.princeton.edu/support/knowledge-base/namd')
     f.write('module purge all\n')
     f.write('module load namd\n')
-    previous = "run" + str(i-1)
-    current = "run" + str(i)    
+    f.write('/software/NAMD/2.13/verbs/charmrun /software/NAMD/2.13/verbs/namd2 +p' + str(proc_no) + ' run_namd_' + str(i) + '.namd > run_namd_' + str(i) + '.log')
+    f.close()
+    
+    # Make some changes to the template namd run file.
+    previous = "run" + str(i-1) # Use previous variable to refer to necessary files from previous simulation.
+    current = "run" + str(i)    # Use the current variable to refer to files to be output from the current simulation.
+
+    # For edge case i = 0, there is no previous run, so simply copy file run.namd as run_namd_0.namd.
     if i == 0:
         input_script = "run.namd"
         os.system('sed "s/run 100000000/run ' + str(steps_per_sim) + '/" ' + input_script + ' > ' + 'run_namd_0.namd')
         # os.system('sed "s/run 100000000/run 100/" ' + input_script + ' > ' + 'run_namd_0.namd')
+    
+    # Otherwise, make a copy of template_run_namd.namd as run_namd_str(i).namd and use the sed command to set some variables.
     else:
         input_script = "run_namd_"+str(i)+".namd"
         firsttimestep = steps_per_sim*i
@@ -44,14 +63,16 @@ for i in range(start,loop):
         os.system('sed -i "s/FIRST_TIMESTEP/' + str(firsttimestep) + '/" ' + input_script)
         os.system('sed -i "s/STEPS_PER_SIM/' + str(steps_per_sim) + '/" ' + input_script)
 
-    f.write('/software/NAMD/2.13/verbs/charmrun /software/NAMD/2.13/verbs/namd2 +p' + str(proc_no) + ' run_namd_' + str(i) + '.namd > run_namd_' + str(i) + '.log')
-    f.close()
-    
+    # Submit this simulation using the sbatch command.
+    # For edge case i = 0, submit run_namd_0.sh.
     if i == start:
         os.system("sbatch " + filename)
+        
+    # After the template_run_namd.namd file has been copied and edited, submit this simulation with a dependency that the last simulation, specified by its job name, must finish before this one starts.
     else:
         os.system("sbatch -d afterok:$(squeue --noheader --format %i --name "+last_jobname+") " + filename)
         print("sbatch -d afterok:$(squeue --noheader --format %i --name "+last_jobname+") " + filename)
         
+    # Set last_jobname for the next run.    
     last_jobname = jobname
     
